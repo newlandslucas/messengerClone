001f16d2433fe7d8addfaaddca7e0a236423be81
lucas-newlands <lucas.newlands@ibm.com>
2023-09-27 14:40:03 -0300

    project start

---
+++ messengerCloneApp/messengerCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  messengerCloneAppApp.swift
+//  messengerCloneApp
+//
+//  Created by Lucas Newlands on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct messengerCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
